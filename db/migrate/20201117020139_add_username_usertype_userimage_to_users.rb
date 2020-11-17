0c15abbc30efb7204e5f33b4745ff6d325dc33da
Ubuntu <ubuntu@ip-172-31-7-4.us-east-2.compute.internal>
2020-11-17 02:07:54 +0000

    usercolumn ok

---
+++ db/migrate/20201117020139_add_username_usertype_userimage_to_users.rb
@@ -0,0 +1,7 @@
+class AddUsernameUsertypeUserimageToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :username, :string
+    add_column :users, :usertype, :string
+    add_column :users, :userimage, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_13_034235) do
+ActiveRecord::Schema.define(version: 2020_11_17_020139) do
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
@@ -69,6 +69,9 @@ ActiveRecord::Schema.define(version: 2020_11_13_034235) do
     t.datetime "locked_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
+    t.string "usertype"
+    t.string "userimage"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
