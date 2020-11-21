211acbdcf0217ee1d55ea04e67ee5cc7a9b952c0
Ubuntu <ubuntu@ip-172-31-7-4.us-east-2.compute.internal>
2020-11-21 10:10:24 +0000

    post ok

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :ensure_correct_user, {only: [:edit, :update, :destroy]}
 
   # GET /posts
   # GET /posts.json
@@ -10,6 +11,7 @@ class PostsController < ApplicationController
   # GET /posts/1
   # GET /posts/1.json
   def show
+    @post = Post.find_by(id: params[:id])
   end
 
   # GET /posts/new
@@ -25,10 +27,11 @@ class PostsController < ApplicationController
   # POST /posts.json
   def create
     @post = Post.new(post_params)
-
+    @post.user_id = current_user.id
+      
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        format.html { redirect_to @post, notice: '投稿できました！' }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new }
@@ -42,7 +45,7 @@ class PostsController < ApplicationController
   def update
     respond_to do |format|
       if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
+        format.html { redirect_to @post, notice: '投稿更新できました！' }
         format.json { render :show, status: :ok, location: @post }
       else
         format.html { render :edit }
@@ -56,8 +59,16 @@ class PostsController < ApplicationController
   def destroy
     @post.destroy
     respond_to do |format|
-      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
+      format.html { redirect_to posts_url, notice: '投稿削除できました！' }
       format.json { head :no_content }
+    end
+  end
+  
+  def ensure_correct_user
+    @post = Post.find_by(id: params[:id])
+    if @post.user_id != current_user.id 
+     flash[:notice] = "権限がありません"
+     redirect_to("/posts")
     end
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_17_023200) do
+ActiveRecord::Schema.define(version: 2020_11_19_013924) do
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2020_11_17_023200) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "content"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
