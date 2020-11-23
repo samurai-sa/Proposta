c68adb3511797d8108232910f80f56917d12206e
Ubuntu <ubuntu@ip-172-31-7-4.us-east-2.compute.internal>
2020-11-23 01:48:24 +0000

    kaminari ok

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,8 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
     @q = Post.ransack(params[:q])
-    @posts = @q.result(distinct: true)
+    @q.sorts = 'updated_at desc' if @q.sorts.empty?
+    @posts = @q.result(distinct: true).page(params[:page]).per(10)
   end
 
   # GET /posts/1
