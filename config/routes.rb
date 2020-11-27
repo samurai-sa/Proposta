bf9ef98198bd69ec5ea8518d0c30a498975b3162
Ubuntu <ubuntu@ip-172-31-7-4.us-east-2.compute.internal>
2020-11-27 06:39:19 +0000

    useredit ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  
-  devise_for :users
+      devise_for :users, controllers: {
+        registrations: "users/registrations",
+      }
+      
   get 'comments/create'
   get 'comments/destroy'
   root 'static_page#home'
@@ -9,7 +11,8 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments, only: [:create, :destroy]
   end
-  
-  
-  
 end
+  
+  
+  
+
