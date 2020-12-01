d64b2ede67b56c45cbeebaba301c73d4c53aee81
Ubuntu <ubuntu@ip-172-31-7-4.us-east-2.compute.internal>
2020-12-01 12:15:40 +0000

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  
+ 
       devise_for :users, controllers: {
         registrations: "users/registrations",
       }
@@ -7,6 +9,7 @@ Rails.application.routes.draw do
   get 'comments/destroy'
   root 'static_page#home'
   get 'static_page/about'
+  resources :mypage, only: [:show]
   
   resources :posts do
     resources :comments, only: [:create, :destroy]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_23_103703) do
+ActiveRecord::Schema.define(version: 2020_12_01_110011) do
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
@@ -51,6 +51,11 @@ ActiveRecord::Schema.define(version: 2020_11_23_103703) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "mypages", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "posts", force: :cascade do |t|
