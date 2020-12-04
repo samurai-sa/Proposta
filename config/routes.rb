12849c8e1b929b9f4a3dfc01802ae1a888e3f985
Ubuntu <ubuntu@ip-172-31-7-4.us-east-2.compute.internal>
2020-12-04 13:54:16 +0000

    header ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  #resources :users, only: [:show]
+  resources :mypages, only: [:show]
   devise_for :users, controllers: {
     registrations: "users/registrations",
   }
