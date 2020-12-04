f8b27d46cde5ec4c8f2e614f7e66948466f0c686
Ubuntu <ubuntu@ip-172-31-7-4.us-east-2.compute.internal>
2020-12-04 04:58:18 +0000

    マイページ未完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,13 @@
 Rails.application.routes.draw do
   
- 
-      devise_for :users, controllers: {
-        registrations: "users/registrations",
-      }
-      
+  #resources :users, only: [:show]
+  devise_for :users, controllers: {
+    registrations: "users/registrations",
+  }
   get 'comments/create'
   get 'comments/destroy'
   root 'static_page#home'
   get 'static_page/about'
-  resources :mypage, only: [:show]
-  
   resources :posts do
     resources :comments, only: [:create, :destroy]
   end
